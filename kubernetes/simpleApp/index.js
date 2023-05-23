2f1dc11da106f9d15090d8cd1f2efb1347219272
Fuad <muhic.fuad.git@gmail.com>
2023-05-23 16:04:21 +0200

    fix

--- kubernetes/simpleApp/index.js
+++ kubernetes/simpleApp/index.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const serverId = process.env.SERVER_ID || "DEFAULT_SERVER"
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/error', (req, res) => {
-    process.exit(1)
+    res.send(`Welcome from server ${serverId}`)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
+
