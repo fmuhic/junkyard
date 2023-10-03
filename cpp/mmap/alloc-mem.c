024637b4d8ae14b660c718df083de49c323eef65
Fuad <muhic.fuad.git@gmail.com>
2023-10-03 15:16:00 +0200

    fix mmap

--- cpp/mmap/alloc-mem.c
+++ cpp/mmap/alloc-mem.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <sys/mman.h>
 
-#define MEM_SIZE 1000000
+#define N 10 
 
 int main() {
 
-    int* mem =  mmap(
+    int* mem = mmap(
         NULL, // hints kernel where to place allocated memory
-        MEM_SIZE, // 1MB
+        N * sizeof(int),
         PROT_READ | PROT_WRITE, // Read + Write
         MAP_PRIVATE | MAP_ANONYMOUS, // Not backed by any file
         0, // No file descriptor
@@ -17,15 +17,16 @@ int main() {
 
     assert(mem != MAP_FAILED && "Memory not allocated" );
 
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < N; i++) {
         mem[i] = i * i;
     }
 
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < N; i++) {
         printf("Hello number %i\n", mem[i]);
     }
 
-    munmap(mem, MEM_SIZE);
+    int err = munmap(mem, N * sizeof(int));
+    assert(err >= 0 && "Unable to deallocate memory" );
 
     return 0;
 }
